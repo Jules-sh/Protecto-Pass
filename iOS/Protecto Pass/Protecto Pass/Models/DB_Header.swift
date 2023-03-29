9299ea5672543b311ff3cdbb361fb95c57cf45d9
Julian Schumacher <Jules.media@outlook.de>
2023-03-29 19:20:34 +0200

    Rename DB Header

--- iOS/Protecto Pass/Protecto Pass/Models/DB_Header.swift
+++ iOS/Protecto Pass/Protecto Pass/Models/DB_Header.swift
@@ -1,48 +1,60 @@
 //
-//  CD_DB_Header.swift
+//  DB_Header.swift
 //  Protecto Pass
 //
-//  Created by Julian Schumacher on 28.03.23.
+//  Created by Julian Schumacher as CD_DB_Header on 28.03.23.
+//
+//  Renamed by Julian Schumacher to DB_Header on 29.03.23.
 //
 
 import Foundation
 
 /// The Header for an encrypted Database containing the
 /// important information about the Database
-internal struct CD_DB_Header {
+internal struct DB_Header {
     
     /// The Enum to choose which
     /// encryption was used to encrypt
     /// the database and correspondingly
     /// has to be used to encrypt it
     internal enum Encryption : String, RawRepresentable {
+        /// Using AES 256 Bit for the Encryption
         case AES256
     }
     
     /// The Enum to declare how the Database is stored.
     internal enum StorageType : String, RawRepresentable {
+        /// Storing this Database in an local encrypted binary File
         case File
+        
+        /// Storing this Database as an encrypted Core Data Instance
         case CoreData
+        
+        /// Storing this Database only local in the Keychain.
         case Keychain
     }
     
     /// The Check String to check if the Decryption of the Database has been successful
-    internal let checkString : String = "Protecto Pass is a great App"
+    internal static let checkString : String = "Protecto Pass is a great App"
     
     ///The Enum telling the App
     ///which Encryption was used to encrypt
     ///the Database
     internal var encryption : Encryption = .AES256
     
+    /// The Enum telling the App how the Database
+    /// is stored.
+    internal var storageType : StorageType = .CoreData
+    
     /// Parses a String and returns a Header
-    internal static func parseString(string : String) -> CD_DB_Header {
+    internal static func parseString(string : String) -> DB_Header {
         let data : [Substring] = string.split(separator: ";")
         // TODO: data[0] or where("encryption:")?
-        return CD_DB_Header(encryption: Encryption(rawValue: String(data[0]))!)
+        return DB_Header(encryption: Encryption(rawValue: String(data[0]))!)
     }
     
     /// Parses this Header to a String which is ready to be stored
     internal func parseHeader() -> String {
-        return "encryption: \(encryption.rawValue);"
+        return "encryption: \(encryption.rawValue); storagetype: \(storageType.rawValue)"
     }
 }

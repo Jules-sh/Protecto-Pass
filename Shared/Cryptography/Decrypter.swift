3909bdb9ee1ead0b625af1ab35614af2602d8d64
Julian Schumacher <professional@julianschumacher.dev>
2023-05-28 19:10:03 +0200

    Update cryptography once again

--- Shared/Cryptography/Decrypter.swift
+++ Shared/Cryptography/Decrypter.swift
@@ -11,15 +11,27 @@ import Foundation
 
 internal enum DecryptionError : Error {
     case errUnlocking
+    case unknownEncryption
 }
 
 internal struct Decrypter {
     
+    /// Decrypter specified for AES 256 Bit Encryption
     private static let aes256 : Decrypter = Decrypter(encryption: .AES256)
     
+    /// Decrypter specified for ChaChaPoly Encryption
     private static let chaChaPoly : Decrypter = Decrypter(encryption: .ChaChaPoly)
     
+    private let encryption : DB_Header.Encryption
+    
+    /// The Database that should be decrypted.
+    /// This is passed with the decrypt Method,
+    /// and is used by the private methods
+    private var db : EncryptedDatabase?
+    
+    /// Returns the correct Decrypter for the passed database
     internal static func getInstance(for db : EncryptedDatabase) -> Decrypter {
+        self.db = db
         if db.header.encryption == .AES256 {
             return aes256
         } else if db.header.encryption == .ChaChaPoly {
@@ -30,18 +42,24 @@ internal struct Decrypter {
     }
     
     private init(encryption : DB_Header.Encryption) {
+        self.encryption = encryption
+    }
+    
+    internal mutating func decrypt() throws -> Database {
+        if encryption == .AES256 {
+            return try decryptAES()
+        } else if encryption == .ChaChaPoly {
+            return try decryptChaChaPoly()
+        } else {
+            throw DecryptionError.unknownEncryption
+        }
+    }
+    
+    private func decryptAES() throws -> Database {
         
     }
     
-    internal func decrypt(db : EncryptedDatabase) throws -> Database {
-        
-    }
-    
-    private func decryptAES(db : EncryptedDatabase) throws -> Database {
-        
-    }
-    
-    private func decryptChaChaPoly(db : EncryptedDatabase) throws -> Database {
+    private func decryptChaChaPoly() throws -> Database {
         
     }
 }
--- Shared/Cryptography/Encrypter.swift
+++ Shared/Cryptography/Encrypter.swift
@@ -9,12 +9,28 @@
 
 import Foundation
 
+internal enum EncryptionError : Error {
+    case errLocking
+    case unknownEncryption
+}
+
+/// Struct used to encypt Databases into encrypted Databases
 internal struct Encrypter {
     
+    /// Encrypter specified for AES 256 Bit Encryption
     private static let aes256 : Encrypter = Encrypter(encryption: .AES256)
     
+    /// Encrypter specified for ChaChaPoly Encryption
     private static let chaChaPoly : Encrypter = Encrypter(encryption: .ChaChaPoly)
     
+    /// The Database that should be encrypted.
+    /// This is passed with the encrypt Method,
+    /// and is used by the private methods
+    private var db : Database?
+    
+    private let encryption : DB_Header.Encryption
+    
+    /// Returns the correct Encrypter for the passed database
     internal static func getInstance(for db : Database) -> Encrypter {
         if db.header.encryption == .AES256 {
             return aes256
@@ -29,7 +45,22 @@ internal struct Encrypter {
         
     }
     
-    internal func encrypt(db : Database) throws -> EncryptedDatabase {
+    internal mutating func encrypt(db : Database) throws -> EncryptedDatabase {
+        self.db = db
+        if db.header.encryption == .AES256 {
+            return try encryptAES()
+        } else if db.header.encryption == .ChaChaPoly {
+            return try encryptChaChaPoly()
+        } else {
+            throw EncryptionError.unknownEncryption
+        }
+    }
+    
+    private func encryptAES() throws -> EncryptedDatabase {
+        
+    }
+    
+    private func encryptChaChaPoly() throws -> EncryptedDatabase {
         
     }
 }

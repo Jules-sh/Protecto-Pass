b8d19f8247ea5c2f15813a53329240fcc78e7c56
Julian Schumacher <professional@julianschumacher.dev>
2023-05-30 14:45:38 +0200

    Implement setup view

--- iOS/Protecto Pass/Protecto Pass/Views/Welcome/SetUpView.swift
+++ iOS/Protecto Pass/Protecto Pass/Views/Welcome/SetUpView.swift
@@ -7,9 +7,25 @@
 
 import SwiftUI
 
+/// The View shown to the User while Loading Data
+/// in the App and setting up everything
 internal struct SetUpView: View {
+    
+    /// The View Context to interact with Core Data System, loading Data if any are there.
+    @Environment(\.managedObjectContext) private var viewContext
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ProgressView()
+                .padding(10)
+            Text("Loading...")
+            Text("This could take a while, depending on your Database count")
+                .multilineTextAlignment(.center)
+                .font(.caption)
+                .padding(.horizontal, 50)
+                .padding(.vertical, 0.5)
+        }
+        
     }
 }
 

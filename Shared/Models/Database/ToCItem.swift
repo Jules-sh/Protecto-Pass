73b0d28f1aab8d23625bb386c6c4d5077c954010
Julian Schumacher <professional@julianschumacher.dev>
2024-02-16 08:05:16 +0100

    Prepare for structure refactoring.
    - Prepare Object Structure for refactoring and change to other storage structure
    - Storage structure will be changed to individual storing, so load-on-demand can be implemented

--- Shared/Models/Database/ToCItem.swift
+++ Shared/Models/Database/ToCItem.swift
@@ -9,4 +9,22 @@ import Foundation
 
 internal struct ToCItem {
     
+    internal var name : String
+    
+    internal var type : ContentType
+    
+    internal var id : UUID
+    
+    internal init(name: String, type: ContentType, id: UUID) {
+        self.name = name
+        self.type = type
+        self.id = id
+    }
+}
+
+internal enum ContentType : String, RawRepresentable {
+    case image
+    case folder
+    case entry
+    case document
 }
